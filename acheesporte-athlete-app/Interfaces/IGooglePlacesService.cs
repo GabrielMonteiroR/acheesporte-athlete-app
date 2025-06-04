a35574b1975788026628307020b39345106a472c
Gabriel <gmonteiro810@gmail.com>
2025-06-04 12:48:38 -0300

    Refactor venue DTOs and add autocomplete response
    This commit introduces new classes and properties for venue and autocomplete responses. The `VenueDto` and `VenueListResponseDto` classes have been restructured with proper namespaces and JSON serialization attributes. A new `AutocompleteResponse` class has been added, featuring a list of predictions and a status property. Additionally, the `IGooglePlacesService` interface has been partially defined for future implementation.

---
+++ acheesporte-athlete-app/Interfaces/IGooglePlacesService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace acheesporte_athlete_app.Interfaces
+{
+    public class IGooglePlacesService
+    {
+        Task<ListPredic>
+    }
+}
