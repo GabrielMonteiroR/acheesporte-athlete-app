a50e9c3471717e8b2208f504245a6b538957b9f3
Gabriel <gmonteiro810@gmail.com>
2025-06-09 17:38:14 -0300

    Add image upload functionality to ImageService
    Introduces the `UploadProfileImageAsync` method for
    asynchronous profile image uploads. Validates file
    existence and extension (JPG, JPEG, PNG), constructs
    a multipart form data request, and processes the
    response to return an `ImageUploadResponseDto` with
    the image URL. Includes error handling for upload
    exceptions.

--- acheesporte-athlete-app/Services/ImageService.cs
+++ acheesporte-athlete-app/Services/ImageService.cs
@@ -1,4 +1,6 @@
 ﻿using acheesporte_athlete_app.Configuration;
+using acheesporte_athlete_app.Dtos.ImageDtos;
+using System.Text.Json;
 
 namespace acheesporte_athlete_app.Services;
 
@@ -13,5 +15,47 @@ public class ImageService
         _apiSettings = apiSettings;
     }
 
+    public async Task<ImageUploadResponseDto> UploadProfileImageAsync(FileResult file)
+    {
+        try
+        {
+            if (file is null || string.IsNullOrWhiteSpace(file.FileName))
+            {
+                throw new ArgumentNullException(nameof(file), "Arquivo não encontrado");
+            }
 
+            var alowedExtensions = new[] { ".jpg", ".jpeg", ".png" };
+
+            var fileExtension = Path.GetExtension(file.FileName).ToLowerInvariant();
+
+            if (!alowedExtensions.Contains(fileExtension))
+            {
+                throw new ArgumentException("Formato de arquivo inválido. Apenas JPG, JPEG e PNG são permitidos.");
+            }
+
+            using var stream = await file.OpenReadAsync();
+            using var content = new MultipartFormDataContent();
+            content.Add(new StreamContent(stream), "image", file.FileName);
+
+            var url = $"{_apiSettings.BaseUrl}{_apiSettings.ImageUploadEndpoint}";
+            var response = await _httpClient.PostAsync(url, content);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Erro ao fazer upload da imagem: {response.ReasonPhrase}");
+            }
+
+            var json = await response.Content.ReadAsStringAsync();
+            var obj = JsonSerializer.Deserialize<JsonElement>(json);
+            return new ImageUploadResponseDto
+            {
+                Image = obj.GetProperty("imageUrl").GetProperty("Image").GetString()
+            };
+
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("Erro ao fazer upload da imagem de perfil", ex);
+        }
+    }
 }
