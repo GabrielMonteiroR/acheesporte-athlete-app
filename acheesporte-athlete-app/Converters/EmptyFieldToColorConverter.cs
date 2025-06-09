94d6b9d8f6a61a04b3d9249b01b4c7d051d96a37
Gabriel <gmonteiro810@gmail.com>
2025-06-09 17:29:52 -0300

    Refactor EmptyFieldToColorConverter and add ZeroToBoolConverter
    Updated the `Convert` method in `EmptyFieldToColorConverter.cs` for improved clarity and efficiency. Adjusted the placement of the `ConvertBack` method and updated the namespace declaration.
    
    Introduced a new `ZeroToBoolConverter` class in `ZeroToBoolConverter.cs`, implementing the `IValueConverter` interface to convert integer values to boolean based on whether they are zero.

--- acheesporte-athlete-app/Converters/EmptyFieldToColorConverter.cs
+++ acheesporte-athlete-app/Converters/EmptyFieldToColorConverter.cs
@@ -1,16 +1,15 @@
 ﻿using System.Globalization;
 
-namespace acheesporte_athlete_app.Converters/
-{
-    public class EmptyFieldToColorConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            string? input = value as string;
-            return string.IsNullOrWhiteSpace(input) ? Colors.Red : Color.FromArgb("#200937");
-        }
+namespace acheesporte_athlete_app.Converters;
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-            => throw new NotImplementedException();
+public class EmptyFieldToColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        string? input = value as string;
+        return string.IsNullOrWhiteSpace(input) ? Colors.Red : Color.FromArgb("#200937");
     }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => throw new NotImplementedException();
 }
