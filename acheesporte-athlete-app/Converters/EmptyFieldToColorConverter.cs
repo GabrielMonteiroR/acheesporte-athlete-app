2d146794c8185995c626f00398ee1f2ff8ecbb02
Gabriel <gmonteiro810@gmail.com>
2025-06-09 17:28:48 -0300

    Add EmptyFieldToColorConverter for data binding
    Introduces the `EmptyFieldToColorConverter` class in the
    `acheesporte_athlete_app.Converters` namespace. This class
    implements the `IValueConverter` interface to provide color
    feedback based on input string validity. The `Convert` method
    returns red for null or whitespace inputs, while a specific
    ARGB color is returned otherwise. The `ConvertBack` method
    is not implemented and throws a `NotImplementedException`.

---
+++ acheesporte-athlete-app/Converters/EmptyFieldToColorConverter.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace acheesporte_athlete_app.Converters/
+{
+    public class EmptyFieldToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string? input = value as string;
+            return string.IsNullOrWhiteSpace(input) ? Colors.Red : Color.FromArgb("#200937");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
