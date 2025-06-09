70221a1132ce0a4e35d9078e9364fd54337a3247
Gabriel <gmonteiro810@gmail.com>
2025-06-09 13:52:53 -0300

    Add CurrentUserDto and initialize UserService class
    Introduced a new data transfer object (DTO) `CurrentUserDto` in `CurrentUserDto.cs` with properties for user details, including JSON serialization attributes. Also added the `UserService` class in `UserService.cs`, which includes a constructor to initialize HTTP client and API settings, though method implementations are not yet complete.

---
+++ acheesporte-athlete-app/Services/UserService.cs
@@ -0,0 +1,23 @@
+﻿using acheesporte_athlete_app.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace acheesporte_athlete_app.Services
+{
+    public class UserService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ApiSettings _apiSettings;
+
+        public UserService(HttpClient httpClient, ApiSettings apiSettings)
+        {
+            _httpClient = httpClient;
+            _apiSettings = apiSettings;
+        }
+
+        public 
+    }
+}
