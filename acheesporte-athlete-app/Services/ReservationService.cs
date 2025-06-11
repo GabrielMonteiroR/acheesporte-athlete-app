65daf302c88bce0054099fced32d6fb43a3bad29
Gabriel <gmonteiro810@gmail.com>
2025-06-11 12:59:59 -0300

    Add ReservationResponseDto and start ReservationService
    Introduced a new DTO, `ReservationResponseDto`, with properties for message and reservations, including JSON serialization attributes. Began implementation of `ReservationService` class with constructor accepting `HttpClient` and `ApiSettings`, though the class is currently incomplete.

---
+++ acheesporte-athlete-app/Services/ReservationService.cs
@@ -0,0 +1,23 @@
+﻿using acheesporte_athlete_app.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace acheesporte_athlete_app.Services
+{
+    public class ReservationService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ApiSettings _apiSettings;
+
+        public ReservationService(HttpClient httpClient, ApiSettings apiSettings)
+        {
+            _httpClient = httpClient;
+            _apiSettings = apiSettings;
+        }
+
+        public async Task<ReservationRes>
+    }
+}
