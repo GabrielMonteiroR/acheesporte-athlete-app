5848e0554d5a49564ec1ccc5dc2decaf4862b38c
Gabriel <gmonteiro810@gmail.com>
2025-06-12 12:27:59 -0300

    Enhance image upload functionality in ImageService
    Updated UploadProfileImageAsync to use HttpRequestMessage
    for image uploads, including authorization token handling.
    Added necessary namespaces for HTTP headers and JSON
    serialization. Improved error handling and response
    deserialization for better security and functionality.

--- acheesporte-athlete-app/Services/ImageService.cs
+++ acheesporte-athlete-app/Services/ImageService.cs
@@ -1,6 +1,7 @@
 ﻿using acheesporte_athlete_app.Configuration;
 using acheesporte_athlete_app.Dtos.ImageDtos;
 using acheesporte_athlete_app.Interfaces;
+using System.Net.Http.Headers;
 using System.Text.Json;
 
 namespace acheesporte_athlete_app.Services;
@@ -21,42 +22,41 @@ public class ImageService : IImageService
         try
         {
             if (file is null || string.IsNullOrWhiteSpace(file.FileName))
-            {
                 throw new ArgumentNullException(nameof(file), "Arquivo não encontrado");
-            }
 
             var alowedExtensions = new[] { ".jpg", ".jpeg", ".png" };
-
             var fileExtension = Path.GetExtension(file.FileName).ToLowerInvariant();
 
             if (!alowedExtensions.Contains(fileExtension))
-            {
                 throw new ArgumentException("Formato de arquivo inválido. Apenas JPG, JPEG e PNG são permitidos.");
-            }
 
             using var stream = await file.OpenReadAsync();
             using var content = new MultipartFormDataContent();
             content.Add(new StreamContent(stream), "image", file.FileName);
 
-            var url = $"{_apiSettings.BaseUrl}{_apiSettings.ImageUploadEndpoint}";
-            var response = await _httpClient.PostAsync(url, content);
+            var token = await SecureStorage.GetAsync("auth_token");
+
+            var request = new HttpRequestMessage(HttpMethod.Post, $"{_apiSettings.BaseUrl}{_apiSettings.ImageUploadEndpoint}");
+            request.Content = content;
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await _httpClient.SendAsync(request);
 
             if (!response.IsSuccessStatusCode)
-            {
                 throw new Exception($"Erro ao fazer upload da imagem: {response.ReasonPhrase}");
-            }
 
             var json = await response.Content.ReadAsStringAsync();
             var obj = JsonSerializer.Deserialize<JsonElement>(json);
+
             return new ImageUploadResponseDto
             {
                 Image = obj.GetProperty("imageUrl").GetProperty("Image").GetString()
             };
-
         }
         catch (Exception ex)
         {
             throw new Exception("Erro ao fazer upload da imagem de perfil", ex);
         }
     }
+
 }
