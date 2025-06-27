e7570d9e4d33354d41ad730fd136acda73a256e9
Gabriel <gmonteiro810@gmail.com>
2025-06-27 16:54:56 -0300

    Add ISportService interface for sports retrieval
    Introduces the `ISportService` interface in the
    `acheesporte_athlete_app.Interfaces` namespace.
    Includes the asynchronous method `GetSportsAsync`
    to return a list of `SportDto` objects.
    Also adds necessary using directives for support.

---
+++ acheesporte-athlete-app/Interfaces/ISportService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace acheesporte_athlete_app.Interfaces;
+
+public interface ISportService
+{
+    Task<List<SportDto>> GetSportsAsync();
+}
