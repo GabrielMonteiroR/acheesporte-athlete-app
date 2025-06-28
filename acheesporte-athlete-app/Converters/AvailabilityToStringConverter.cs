6b624bdb22b431983c6dc86c4724e687b767d401
Gabriel <gmonteiro810@gmail.com>
2025-06-28 10:48:51 -0300

    Add AvailabilityToStringConverter for venue availability
    Introduces the `AvailabilityToStringConverter` class in the
    `acheesporte_athlete_app.Converters` namespace. This class
    implements the `IValueConverter` interface to convert
    `VenueAvailabilityDto` objects into formatted strings that
    display the start time and price. The `Convert` method is
    implemented, while `ConvertBack` throws a
    `NotImplementedException`. Additional using directives are
    included for necessary namespaces.

---
+++ acheesporte-athlete-app/Converters/AvailabilityToStringConverter.cs
@@ -0,0 +1,20 @@
+﻿using acheesporte_athlete_app.Dtos.Venues;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace acheesporte_athlete_app.Converters;
+
+public class AvailabilityToStringConverter : IValueConverter
+{
+    public object Convert(object value, Type t, object p, CultureInfo c)
+    {
+        if (value is VenueAvailabilityDto a)
+            return $"{a.StartDate:HH:mm} - R$ {a.Price:F2}";
+        return string.Empty;
+    }
+    public object ConvertBack(object v, Type t, object p, CultureInfo c) => throw new NotImplementedException();
+}
